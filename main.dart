e06a3d30ad5d36c45cb6c7635f9faf2055312e7e
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-11 17:46:58 +0500

    Assignment Operators

--- main.dart
+++ main.dart
@@ -4,14 +4,28 @@
 //Equity and Relational operators
 
 
-var a = 100;
-var b = 200;
-print( a == b );
-print( a != b );
-print( a <= b );
-print( a >= b );
-print( a < b );
-print( a > b );
+// var a = 100;
+// var b = 200;
+// print( a == b );
+// print( a != b );
+// print( a <= b );
+// print( a >= b );
+// print( a < b );
+// print( a > b );
+
+
+
+
+//Assignment Operator
+
+var n = 15;
+n += 5;
+n -= 5;
+n *= 5;
+print(n);
+
+
+
 
 }
 
