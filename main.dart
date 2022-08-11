052b60243eb3db4b1825d7a3c6e9175cd11b2a07
Hassan Ul Haq <81625175+Hassanulhaq52@users.noreply.github.com>
2022-08-11 17:37:22 +0500

    Equity and Relational operator

---
+++ main.dart
@@ -0,0 +1,17 @@
+ main() {
+
+
+//Equity and Relational operators
+
+
+var a = 100;
+var b = 200;
+print( a == b );
+print( a != b );
+print( a <= b );
+print( a >= b );
+print( a < b );
+print( a > b );
+
+}
+
